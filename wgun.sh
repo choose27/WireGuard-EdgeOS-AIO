35c9c591b35f1958ff055dbb0a0085961daf19a9
choose27 <choosemeftw@gmail.com>
2021-05-21 11:36:37 -0400

    Update wgun.sh

--- wgun.sh
+++ wgun.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ## Install WireGuard
-curl https://files.tsok.ml/s/XdBKnaJwREoCQz7/download >> install.sh
-chmod a+x install.sh
-/bin/bash install.sh remove
+curl https://raw.githubusercontent.com/mafredri/vyatta-wireguard-installer/master/wireguard.sh >> /config/WireGuardAIO/install.sh
+chmod a+x /config/WireGuardAIO/install.sh
+/bin/bash /config/WireGuardAIO/install.sh remove
 wait
 ## Set up the Interface
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
@@ -18,10 +18,13 @@ sudo apt-get update
 sudo apt-get install qrencode -y
 sudo dpkg --remove wireguard
 sudo dpkg --purge wireguard
-rm nextip.txt
-rm wg.public
-rm wgadd.sh
-rm install.sh
+rm /config/WireGuardAIO/nextip.txt
+rm /config/WireGuardAIO/wg.public
+rm /config/WireGuardAIO/wgadd.sh
+rm /config/WireGuardAIO/install.sh
+rm /config/WireGuardAIO/*.peer
+rm /config/WireGuardAIO/*.conf
 rm /config/auth/wg.key
+
 sudo apt remove qrencode -y
 sudo apt autoremove -y
