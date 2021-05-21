0fcf9cf206dcd38c099ae095cabf3a8acf1323e1
choose27 <choosemeftw@gmail.com>
2021-05-21 11:35:59 -0400

    fix script path
    create directory if doesn't exist and fixed script path

--- wg.sh
+++ wg.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 ## Install WireGuard
-curl https://raw.githubusercontent.com/mafredri/vyatta-wireguard-installer/master/wireguard.sh >> install.sh
-chmod a+x install.sh
-/bin/bash install.sh install
+if [ ! -d /config/WireGuardAIO ]; then
+  mkdir -p /config/WireGuardAIO;
+fi
+curl https://raw.githubusercontent.com/mafredri/vyatta-wireguard-installer/master/wireguard.sh >> /config/WireGuardAIO/install.sh
+chmod a+x /config/WireGuardAIO/install.sh
+/bin/bash /config/WireGuardAIO/install.sh install
 
 ## Set up the Interface
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
@@ -16,7 +19,7 @@ chmod a+x install.sh
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper save
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
 ## Generate Keys
-wg genkey | tee /config/auth/wg.key | wg pubkey >  wg.public
+wg genkey | tee /config/auth/wg.key | wg pubkey >  /config/WireGuardAIO/wg.public
 
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set interfaces wireguard wg0 address 10.254.254.1/24
@@ -37,12 +40,14 @@ sudo apt-get update
 sudo apt-get install qrencode -y
 
 ## Prep the automatic peer configuration
-curl https://files.tsok.ml/s/AFFKEKEmm9iBJgG/download >> wgadd.sh
-routerpubkey=$(cat wg.public)
-sed -i s[rOuTeRPUblicKey12345[$routerpubkey[ wgadd.sh
+curl https://raw.githubusercontent.com/choose27/Wireguard-EdgeOS-AIO/main/wgadd.sh?token=ABGY2QHBW7BJ2H7LE2FP2S3AU7H66 >> /config/WireGuardAIO/wgadd.sh
+routerpubkey=$(cat /config/WireGuardAIO/wg.public)
+sed -i s[<pubkey>[$routerpubkey[ /config/WireGuardAIO/wgadd.sh
 echo "Enter your endpoint domain or ip"
-read endpoint
-sed -i s[yourdomain.com[$endpoint[ wgadd.sh
-chmod a+x wgadd.sh
-echo 2 >> nextip.txt
+read -p 'Enter your endpoint domain or ip: ' -e -i 'mydomainorpublicip.com' endpoint
+sed -i s[<ep>[$endpoint[ /config/WireGuardAIO/wgadd.sh
+read -p 'Enter DNS server(s): ' -e -i '1.1.1.1,1.0.0.1' wgdns
+sed -i s[<dns>[$wgdns[ /config/WireGuardAIO/wgadd.sh
+chmod a+x /config/WireGuardAIO/wgadd.sh
+echo 2 >> /config/WireGuardAIO/nextip.txt
 echo "All done! Run './wgadd.sh wg0 peername' to generate a QR code for the app"
