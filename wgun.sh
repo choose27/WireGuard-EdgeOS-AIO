eb9bfeeda7a8d897912b8e28542e3d7b81c13d17
choose27 <choosemeftw@gmail.com>
2021-05-21 22:40:51 -0400

    Cleaned up

--- wgun.sh
+++ wgun.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
-## Install WireGuard
+## Remove WireGuard
 curl https://raw.githubusercontent.com/mafredri/vyatta-wireguard-installer/master/wireguard.sh >> /config/WireGuardAIO/install.sh
 chmod a+x /config/WireGuardAIO/install.sh
 /bin/bash /config/WireGuardAIO/install.sh remove
-## Set up the Interface
+
+## Remove WAN_LOCAL rule
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete interfaces wireguard
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete firewall name WAN_LOCAL rule 540
-#### get qrencode
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete system package repository
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper save
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
 
-sudo apt-get update
-sudo apt-get install qrencode -y
-sudo dpkg --remove wireguard
-sudo dpkg --purge wireguard
-rm -R /config/WireGuardAIO
-rm /config/auth/wg.key
-
+## Remove qrcode
 sudo apt remove qrencode -y
 sudo apt autoremove -y
+
+## Clean up space
+sudo apt autoclean
+
+## Remove WireGuardAIO folder
+rm -R /config/WireGuardAIO
+
+## Remove key
+rm /config/auth/wg.key
+
