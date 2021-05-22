5e27f566fd5c38cd1c3e8a61450b6c5d19b43ce1
choose27 <choosemeftw@gmail.com>
2021-05-22 00:38:27 -0400

    Update wgun.sh

--- wgun.sh
+++ wgun.sh
@@ -3,18 +3,21 @@
 if [[ $(id -g -n) != 'vyattacfg' ]]; then
 	exec sg vyattacfg -c "$0 $@"
 fi
+
+config=/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper
+
 ## Remove WireGuard
 curl https://raw.githubusercontent.com/mafredri/vyatta-wireguard-installer/master/wireguard.sh >> /config/WireGuardAIO/install.sh
 chmod a+x /config/WireGuardAIO/install.sh
 /bin/bash /config/WireGuardAIO/install.sh remove
 
 ## Remove WAN_LOCAL rule
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete firewall name WAN_LOCAL rule 540
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete system package repository
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper save
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
+config begin
+config delete firewall name WAN_LOCAL rule 540
+config delete system package repository
+config commit
+config save
+config end
 
 ## Remove qrcode
 sudo apt remove qrencode -y
