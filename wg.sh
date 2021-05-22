efa31843b9052f2f229f1d730c43e7bdc65d9234
choose27 <choosemeftw@gmail.com>
2021-05-22 00:48:06 -0400

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -20,23 +20,23 @@ chmod a+x /config/WireGuardAIO/install.sh
 wg genkey | tee /config/auth/wg.key | wg pubkey >  /config/WireGuardAIO/wg.public
 
 ## Add WAN_LOCAL rule
-config begin
-config set firewall name WAN_LOCAL rule 540 action accept
-config set firewall name WAN_LOCAL rule 540 description 'WireGuard'
-config set firewall name WAN_LOCAL rule 540 destination port 51820
-config set firewall name WAN_LOCAL rule 540 protocol udp
+$config begin
+$config set firewall name WAN_LOCAL rule 540 action accept
+$config set firewall name WAN_LOCAL rule 540 description 'WireGuard'
+$config set firewall name WAN_LOCAL rule 540 destination port 51820
+$config set firewall name WAN_LOCAL rule 540 protocol udp
 ## Add repo and WAN_LOCAL Rules
-config set system package repository stretch components 'main contrib non-free'
-config set system package repository stretch distribution stretch
-config set system package repository stretch url http://http.us.debian.org/debian
+$config set system package repository stretch components 'main contrib non-free'
+$config set system package repository stretch distribution stretch
+$config set system package repository stretch url http://http.us.debian.org/debian
 ## Add the interface
-config set interfaces wireguard wg0 address 10.254.254.1/24
-config set interfaces wireguard wg0 listen-port 51820
-config set interfaces wireguard wg0 route-allowed-ips true
-config set interfaces wireguard wg0 private-key /config/auth/wg.key
-config commit
-config save
-config end
+$config set interfaces wireguard wg0 address 10.254.254.1/24
+$config set interfaces wireguard wg0 listen-port 51820
+$config set interfaces wireguard wg0 route-allowed-ips true
+$config set interfaces wireguard wg0 private-key /config/auth/wg.key
+$config commit
+$config save
+$config end
 
 ## Grab uninstaller
 curl https://raw.githubusercontent.com/choose27/WireGuard-EdgeOS-AIO/main/wgun.sh >> /config/WireGuardAIO/wgun.sh
